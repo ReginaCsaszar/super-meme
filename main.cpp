049fb60fe301057067bdf412d6062a1edcb09679
ReginaCsaszar <regina.csaszar@gmail.com>
2017-09-23 18:22:59 +0200

    added user input

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    for (int i = 1; i < argc; i++) {
-        cout << "Hello, " << argv[i] << "!" << endl;
-    }
+int main() {
+    string name;
+    cout << "Name? " << endl;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
     return 0;
 }
-
-/* second solution also works:
- *
- * #include <vector>
- *
- * vector<string> names(argv, argv + argc);
- * names.erase(names.begin());
- * for (string& name : names) {
- * cout << "Hello, " << name << "!" << endl;
- * }
- */
